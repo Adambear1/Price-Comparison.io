d31f18566ba511bb6d8a39939f3aa174398fd831
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2020-05-09 16:43:55 -0700

    update

--- server.js
+++ server.js
@@ -1,9 +1,45 @@
-Dependencies
 var express = require("express");
-
+var path = require("path");
+var fs = require('fs')
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
+app.get('/style.css', function (req, res) {
+    res.sendFile(__dirname + "/" + "style.css");
+});
+app.get('/logic.js', function (req, res) {
+    res.sendFile(__dirname + "/" + "logic.js");
+});
+app.get('/api/input', (req, res) => {
+    res.sendFile(path.join(__dirname, 'db.json'))
+})
+
+app.post('/api/input', (req, res) => {
+    var newData = req.body;
+    console.log(newData);
+    newData.itemName = newData.item.replace(/\s+/g, "").toLowerCase()
+    fs.readFile('db.json', 'utf-8', (err, data) => {
+        var item = JSON.parse(data);
+        item.push(newData);
+        fs.writeFile('db.json', JSON.stringify(item), err => {
+            if (err) {
+                throw err
+            }
+            res.json(newData)
+        })
+    })
+})
+
+
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
